97fe008e869654b563e866dde6b8591a04598ac0
helianthimius <homami.h78@gmail.com>
2023-10-06 22:43:42 -0300

    Add README and comments

---
+++ Assets/README.md
@@ -0,0 +1,48 @@
+# Assignment 1 - CSCI6609
+Helia Homami - B00975927
+
+## Repository
+[GitHub](https://github.com/helianthimius/CSCI6609-Lab)
+
+## Project Overview
+### Theme
+Space
+
+### Main Character
+White Spaceship (Cutter)
+
+### Sensors
+* **Touch**: Swipe up/down to change the altitude
+* **Gyroscope**: Rotate device with yourself to change the direction
+* **Accelerator**: Shake to change color
+* **Step Counter**: Step to move forward
+
+### Other Objects
+* Floor (Terrain)
+* Light
+* 2 Buildings (4 Cubes)
+* 2 Other Spaceships (Red and Green)
+* 4 Dead Trees (2 Different Rampikes)
+
+## Citations and References
+* [Space sky](https://assetstore.unity.com/packages/3d/environments/landscapes/lunar-landscape-3d-132614)
+* [Space assets](https://assetstore.unity.com/packages/3d/vehicles/space/voxel-space-ships-109876)
+* [Setup VSCode for unity](https://code.visualstudio.com/docs/other/unity)
+* [Force app to run in landscape mode](https://discussions.unity.com/t/landscape-mode-only/114273)
+* [Changing color of child game object](https://discussions.unity.com/t/renderer-material-color-not-changing-color-of-prefab/246469)
+* [Touch docs (mobile input)](https://docs.unity3d.com/2022.1/Documentation/Manual/MobileInput.html)
+* [Touch docs](https://docs.unity3d.com/2022.1/Documentation/ScriptReference/Touch.html)
+* [Get permission in android](https://docs.unity3d.com/Manual/android-RequestingPermissions.html)
+* [Override android manifest for adding permission](https://docs.unity3d.com/Manual/overriding-android-manifest.html#creating-a-template-android-manifest-file)
+* [Add tree](https://docs.unity3d.com/Manual/tree-FirstTree.html)
+* [Find game object](https://docs.unity3d.com/ScriptReference/GameObject.Find.html)
+* [Gyroscope docs](https://docs.unity3d.com/ScriptReference/Input-gyro.html)
+* [Change color of material](https://docs.unity3d.com/ScriptReference/Material.SetColor.html)
+* [Generate random value](https://docs.unity3d.com/ScriptReference/Random-value.html)
+* [Transform object direction](https://docs.unity3d.com/ScriptReference/Transform-eulerAngles.html)
+* [Transform position](https://docs.unity3d.com/ScriptReference/Transform-position.html)
+* [Move object in direction](https://docs.unity3d.com/ScriptReference/Transform.Translate.html)
+* [How to use `var` keyword](https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/implicitly-typed-local-variables)
+* [Add script component to game object](https://sharpcoderblog.com/blog/unity-3d-how-to-attach-a-script-or-a-component-to-a-game-object)
+* [Use steps counter](https://www.reddit.com/r/Unity3D/comments/wjrg5q/how_to_use_the_pedometerstepcounter_sensor/)
+* [Exact post for steps counter](https://www.reddit.com/r/Unity3D/comments/wjrg5q/how_to_use_the_pedometerstepcounter_sensor/jcesp2h/?utm_source=share&utm_medium=mweb3x&utm_name=mweb3xcss&utm_term=1&utm_content=share_button)
--- Assets/SensorComponent.cs
+++ Assets/SensorComponent.cs
@@ -6,37 +6,72 @@ using UnityEngine.Android;
 
 public class SensorComponent : MonoBehaviour
 {
+    // TOUCH
+    // The current altitude of spaceship
     float altitude;
 
+    // ACCELERATOR
+    // The threshold for detecting device is shaking strong enough
     float shakeDetectionThreshold = 2f;
+
+    // The minimum interval between two shakes
     float shakeInterval = 2f;
-    Vector3 lastAcceleration;
+
+    // The acceleration of the device in the previous frame
+    Vector3 lastAcceleration = new(0, 0, 0);
+
+    // How long has been taken since the last shake
     float timeSinceLastShake = 0;
 
+    // STEP COUNTER
+    // The last measured steps count by the device
     int lastStepCount = 0;
+
+    // Changes the color of spaceship
     void ChangeColor()
     {
+        // Generates random color in RGBa with random values
+        // https://docs.unity3d.com/ScriptReference/Random-value.html
         Color randomColor = new Color(Random.value, Random.value, Random.value);
+
+        // It "finds" the exact object (child of current object), and change its color by using its renderer
+        // https://discussions.unity.com/t/renderer-material-color-not-changing-color-of-prefab/246469
+        // https://docs.unity3d.com/ScriptReference/GameObject.Find.html
+        // https://docs.unity3d.com/ScriptReference/Material.SetColor.html
         GameObject.Find("cutter").GetComponent<MeshRenderer>().material.SetColor("_Color", randomColor);
     }
 
     void Awake()
     {
+        // It gets permission from the user (player) for getting activity information the moment the application opens (just once)
+        // https://docs.unity3d.com/Manual/android-RequestingPermissions.html
+        // https://docs.unity3d.com/Manual/overriding-android-manifest.html#creating-a-template-android-manifest-file
         Permission.RequestUserPermission("android.permission.ACTIVITY_RECOGNITION");
     }
 
     void Start()
     {
+        // https://docs.unity3d.com/ScriptReference/Input-gyro.html
         Input.gyro.enabled = true;
+
+        // https://www.reddit.com/r/Unity3D/comments/wjrg5q/how_to_use_the_pedometerstepcounter_sensor/jcesp2h/?utm_source=share&utm_medium=mweb3x&utm_name=mweb3xcss&utm_term=1&utm_content=share_button
         InputSystem.EnableDevice(AndroidStepCounter.current);
         AndroidStepCounter.current.MakeCurrent();
     }
 
     void Update()
     {
+        // TOUCH
+        // Checks if user is touching the screen
         if (Input.touchCount > 0)
         {
+            // https://docs.unity3d.com/2022.1/Documentation/ScriptReference/Touch.html
+            // Multi-touch is handled. Just handling the first finger.
             var touch = Input.GetTouch(0);
+
+            // At the start (and end) of finger swiping, the altitude of spaceship should be stored to be used for adjusting the relative
+            // altitude during moving phase of swipe.
+            // "Ended" is considered for handling when player finger goes out of the screen.
             if (touch.phase == UnityEngine.TouchPhase.Began || touch.phase == UnityEngine.TouchPhase.Ended)
             {
                 altitude = transform.position.y;
@@ -44,7 +79,13 @@ public class SensorComponent : MonoBehaviour
             else if (touch.phase == UnityEngine.TouchPhase.Moved)
             {
                 var delta = (touch.position - touch.rawPosition).y;
+                // https://docs.unity3d.com/ScriptReference/Transform-position.html
+                // The value of 0.05 is used to decrease the speed of spaceship movement. When changing the touch position,
+                // the spaceship's rate of altitude change was too high, so I decreased it by 0.05 rate.
                 transform.position = new Vector3(transform.position.x, delta * 0.05f + altitude, transform.position.z);
+
+                // The spaceship's initial position on the y-axis is at 30 units. To make the movements more logical, it can reach
+                // a maximum altitude of 50 and a minimum altitude of 10.
                 if (transform.position.y > 50)
                 {
                     transform.position = new Vector3(transform.position.x, 50, transform.position.z);
@@ -56,25 +97,49 @@ public class SensorComponent : MonoBehaviour
             }
         }
 
+        // GYROSCOPE
+        // Spaceship should be rotated only through the y-axis, so the angle with x and z axis are always 0.
+        // By try and error the correct axis of device gyroscope attitude was found.
+        // https://docs.unity3d.com/ScriptReference/Transform-eulerAngles.html
+        // https://docs.unity3d.com/ScriptReference/Input-gyro.html
         transform.eulerAngles = new Vector3(0, -Input.gyro.attitude.eulerAngles.z, 0);
 
+        //ACCELERATOR 
+        // Reads the device acceleration
         var accelerometerReading = Input.acceleration;
-        Vector3 deltaAcceleration = accelerometerReading - lastAcceleration;
-        float accelerationForce = deltaAcceleration.sqrMagnitude;
 
+        // Calculates delta acceleration 
+        Vector3 deltaAcceleration = accelerometerReading - lastAcceleration;
+
+        // Calculates the magnitude ^ 2 of delta acceleration vector. The squared value is used because
+        // calculating magnitude itself is slower and unnecessary for comparison
+        var accelerationForce = deltaAcceleration.sqrMagnitude;
+
+        // Updates time since last shake
         timeSinceLastShake += Time.deltaTime;
 
+
+        // Checks if delta acceleration is big enough and it has been taken appropriate time from the last shake
         if (accelerationForce >= shakeDetectionThreshold && timeSinceLastShake > shakeInterval)
         {
             ChangeColor();
             timeSinceLastShake = 0;
         }
+
+        // Updates the last acceleration
         lastAcceleration = accelerometerReading;
 
-        if (lastStepCount < AndroidStepCounter.current.stepCounter.ReadValue())
+        // STEP COUNTER
+        var stepCountRead = AndroidStepCounter.current.stepCounter.ReadValue();
+        // Checks if previous measured steps count has increased or not
+        if (lastStepCount < stepCountRead)
         {
+            // https://docs.unity3d.com/ScriptReference/Transform.Translate.html
+            // Moves the spaceship 10 units forward
             transform.Translate(Vector3.forward * 10f);
-            lastStepCount = AndroidStepCounter.current.stepCounter.ReadValue();
+
+            //Updates the last step count
+            lastStepCount = stepCountRead;
         }
     }
 }
